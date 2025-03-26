3fb17f0bc5b764bd1bdc193e2194a3a34c7bf9e1
aaa <aaa@DESKTOP-5GLG45L>
2025-03-26 18:10:44 +0100

    modificacion del punto 24

--- pronombre1nombre2/src/pronombre1nombre2/Holamundo.java
+++ pronombre1nombre2/src/pronombre1nombre2/Holamundo.java
@@ -2,6 +2,6 @@ package pronombre1nombre2;
 
 public class Holamundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo este es el quinto cambio! Este dara error1");
+        System.out.println("Hola Mundo esta es la modificacion del punto 24");
     }
 }
