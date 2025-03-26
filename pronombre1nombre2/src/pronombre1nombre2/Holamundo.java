72a71d0ea58cc04d3c347c0056a83b2fb30a8763
aaa <aaa@DESKTOP-5GLG45L>
2025-03-26 15:06:46 +0100

    4º cambio, remoto tambien.

--- pronombre1nombre2/src/pronombre1nombre2/Holamundo.java
+++ pronombre1nombre2/src/pronombre1nombre2/Holamundo.java
@@ -2,6 +2,6 @@ package pronombre1nombre2;
 
 public class Holamundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo tercer cambio, este remoto!");
+        System.out.println("Hola Mundo este es el cuarto cambio!");
     }
 }
